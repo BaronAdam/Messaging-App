839bc9e8e5e957d38a21a3de2d7d85714d4c7234
Adam <adamo.b1997@gmail.com>
2020-07-20 19:07:34 +0200

    Add AuthController + User Register method

---
+++ Messaging-App.Api/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Messaging_App.Domain;
+using Messaging_App.Infrastructure.DTOs;
+using Messaging_App.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Messaging_App.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repository;
+
+        public AuthController(IAuthRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            if (await _repository.UserExists(userForRegisterDto.Username))
+            {
+                return BadRequest("Username already exists");
+            }
+
+            if (await _repository.EmailExists(userForRegisterDto.Email))
+            {
+                return BadRequest("Email already registered");
+            }
+
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.Username,
+                Email = userForRegisterDto.Email
+            };
+
+            var createdUser = await _repository.Register(userToCreate, userForRegisterDto.Password);
+
+            return StatusCode(201);
+        }
+    }
+}
---
+++ Messaging-App.Infrastructure/DTOs/UserForRegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Messaging_App.Infrastructure.DTOs
+{
+    public class UserForRegisterDto
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
