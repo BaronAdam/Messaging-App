8b2722fd215f99d311eed2ba556fe80e81758562
Adam <adamo.b1997@gmail.com>
2020-07-20 11:13:06 +0200

    Add Register method implementation to Auth Repository

---
+++ Messaging-App.Infrastructure/Persistence/AuthRepository.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using Messaging_App.Domain;
+
+namespace Messaging_App.Infrastructure.Persistence
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly AppDbContext _context;
+
+        public AuthRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<User> Register(User user, string password)
+        {
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public async Task<User> Login(string username, string email, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<bool> EmailExists(string email)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
