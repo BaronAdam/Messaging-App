01864a8fec067c40253e129446f57a480b416ec4
Adam <adamo.b1997@gmail.com>
2020-07-20 10:13:06 +0200

    Add user model + migrations

--- Messaging-App.Infrastructure/Persistence/AppDbContext.cs
+++ Messaging-App.Infrastructure/Persistence/AppDbContext.cs
@@ -1,3 +1,4 @@
+using Messaging_App.Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace Messaging_App.Infrastructure.Persistence
@@ -5,5 +6,7 @@ namespace Messaging_App.Infrastructure.Persistence
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+        public DbSet<User> Users { get; set; }
     }
 }
