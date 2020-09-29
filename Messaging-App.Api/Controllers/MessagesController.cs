b4c8b61055a770a62eeb9f4b4dcc1b253108a67d
Adam <adamo.b1997@gmail.com>
2020-09-29 20:56:18 +0200

    Fix method of returning messages

--- Messaging-App.Api/Controllers/MessagesController.cs
+++ Messaging-App.Api/Controllers/MessagesController.cs
@@ -11,7 +11,6 @@ using Messaging_App.Api.Helpers;
 using Messaging_App.Domain.DTOs;
 using Messaging_App.Domain.Models;
 using Messaging_App.Infrastructure.Interfaces;
-using Messaging_App.Infrastructure.Parameters;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
@@ -23,15 +22,16 @@ namespace Messaging_App.Api.Controllers
     [ApiController]
     public class MessagesController : ControllerBase
     {
-        private readonly IMessageRepository _messageRepository;
-        private readonly IMessageGroupRepository _groupRepository;
         private readonly IAppRepository _appRepository;
-        private readonly IUserRepository _userRepository;
-        private readonly IMapper _mapper;
         private readonly Cloudinary _cloudinary;
+        private readonly IMessageGroupRepository _groupRepository;
+        private readonly IMapper _mapper;
+        private readonly IMessageRepository _messageRepository;
+        private readonly IUserRepository _userRepository;
 
         public MessagesController(IMessageRepository messageRepository, IMapper mapper, IAppRepository appRepository,
-            IMessageGroupRepository groupRepository, IUserRepository userRepository, IOptions<CloudinarySettings> cloudinaryOptions)
+            IMessageGroupRepository groupRepository, IUserRepository userRepository,
+            IOptions<CloudinarySettings> cloudinaryOptions)
         {
             _messageRepository = messageRepository;
             _mapper = mapper;
@@ -40,13 +40,13 @@ namespace Messaging_App.Api.Controllers
             _userRepository = userRepository;
 
             var account = new Account(
-                cloudinaryOptions.Value.ApiKey, 
-                cloudinaryOptions.Value.ApiKey, 
+                cloudinaryOptions.Value.ApiKey,
+                cloudinaryOptions.Value.ApiKey,
                 cloudinaryOptions.Value.ApiSecret);
-            
+
             _cloudinary = new Cloudinary(account);
         }
-        
+
         [HttpGet("{id}", Name = "GetMessage")]
         [ProducesResponseType(typeof(Message), (int) HttpStatusCode.OK)]
         [ProducesResponseType((int) HttpStatusCode.Unauthorized)]
@@ -67,14 +67,13 @@ namespace Messaging_App.Api.Controllers
         [ProducesResponseType(typeof(IEnumerable<MessageToReturnDto>), (int) HttpStatusCode.OK)]
         [ProducesResponseType((int) HttpStatusCode.Unauthorized)]
         [ProducesResponseType((int) HttpStatusCode.InternalServerError)]
-        public async Task<IActionResult> GetMessageThread(int userId, int groupId, 
-            [FromQuery] MessageParameters messageParameters)
+        public async Task<IActionResult> GetMessageThread(int userId, int groupId)
         {
             if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value)) return Unauthorized();
 
             if (await _groupRepository.GetUserMessageGroup(userId, groupId) == null) return Unauthorized();
 
-            var messages = await _messageRepository.GetMessageThread(userId, groupId, messageParameters);
+            var messages = await _messageRepository.GetMessageThread(userId, groupId);
 
             var messageThread = _mapper.Map<IEnumerable<MessageToReturnDto>>(messages);
 
@@ -118,15 +117,15 @@ namespace Messaging_App.Api.Controllers
                 messageForCreationDto.Url = result.Url.ToString();
                 messageForCreationDto.PublicId = result.PublicId;
             }
-            
+
             _appRepository.Add(message);
 
             if (!await _appRepository.SaveAll()) return StatusCode((int) HttpStatusCode.InternalServerError);
-            
+
             var messageToReturn = _mapper.Map<MessageToReturnDto>(message);
             return CreatedAtRoute("GetMessage", new {userId, id = message.Id}, messageToReturn);
         }
-        
+
         [HttpGet]
         [ProducesResponseType(typeof(List<MessageGroupToReturnDto>), (int) HttpStatusCode.OK)]
         [ProducesResponseType((int) HttpStatusCode.Unauthorized)]
@@ -138,7 +137,7 @@ namespace Messaging_App.Api.Controllers
             var messageGroups = await _groupRepository.GetAllMessageGroupsForUser(userId);
 
             var groups = new List<MessageGroupToReturnDto>();
-            
+
             foreach (var messageGroup in messageGroups)
             {
                 var message = await _messageRepository.GetLastMessage(messageGroup.Id) ?? new Message
@@ -151,7 +150,7 @@ namespace Messaging_App.Api.Controllers
                 };
 
                 string groupName;
-                
+
                 if (!messageGroup.IsGroup)
                 {
                     var ids = await _groupRepository.GetUserIdsForGroup(messageGroup.Id);
@@ -167,10 +166,7 @@ namespace Messaging_App.Api.Controllers
 
                 var content = message.Content;
 
-                if (content.Length > 60)
-                {
-                    content = content.Substring(0, 27) + "...";
-                }
+                if (content.Length > 60) content = content.Substring(0, 27) + "...";
 
                 var lastName = message.Sender.Id == userId ? "You" : message.Sender.Name;
 
