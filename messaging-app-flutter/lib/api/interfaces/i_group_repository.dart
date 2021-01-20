10937cd0469fb9c7354a93c8cac21ec1ff5cd94f
Adam <adamo.b1997@gmail.com>
2021-01-20 10:01:23 +0100

    add GroupRepository

---
+++ messaging-app-flutter/lib/api/interfaces/i_group_repository.dart
@@ -0,0 +1,14 @@
+import 'package:messaging_app_flutter/api/objects/error_status.dart';
+import 'package:messaging_app_flutter/api/objects/member_and_friends.dart';
+import 'package:messaging_app_flutter/api/objects/members_and_admins.dart';
+
+abstract class IGroupRepository {
+  Future<ErrorStatus> addGroup(String name);
+  Future<ErrorStatus> changeGroupName(int groupId, String name);
+  Future<List<int>> getMembersForGroup(int groupId);
+  Future<List<int>> getAdminsForGroup(int groupId);
+  Future<ErrorStatus> addMembersToGroup(int groupId, List<int> userIds);
+  Future<ErrorStatus> changeAdminStatus(int groupId, int memberId);
+  Future<MembersAndAdmins> getMembersAndAdmins(int groupId);
+  Future<MembersAndFriends> getFriendsAndMembers(int groupId);
+}
--- messaging-app-flutter/lib/api/repositories/group_repository.dart
+++ messaging-app-flutter/lib/api/repositories/group_repository.dart
@@ -1,98 +1,235 @@
+import 'package:injectable/injectable.dart';
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-import 'package:messaging_app_flutter/DTOs/member_and_friends_dto.dart';
-import 'package:messaging_app_flutter/DTOs/members_and_admins_dto.dart';
-import 'package:messaging_app_flutter/DTOs/user_for_single_dto.dart';
-import 'package:messaging_app_flutter/api/group.dart';
-import 'package:messaging_app_flutter/api/user.dart';
+import 'package:messaging_app_flutter/api/interfaces/i_auth_repository.dart';
+import 'package:messaging_app_flutter/api/interfaces/i_group_repository.dart';
+import 'package:messaging_app_flutter/api/objects/error_status.dart';
+import 'package:messaging_app_flutter/api/objects/member_and_friends.dart';
+import 'package:messaging_app_flutter/api/objects/members_and_admins.dart';
+import 'package:messaging_app_flutter/constants.dart';
 
-class GroupRepository {
-  static Future<MembersAndAdminsDto> getMembersAndAdminInfo(
-      userId, groupId, token) async {
-    var requestMembers = await Group.getMembersForGroup(userId, groupId, token);
-    var requestAdmins = await Group.getAdminsForGroup(userId, groupId, token);
+@Injectable(as: IGroupRepository)
+class GroupRepository implements IGroupRepository {
+  IAuthRepository _authRepository;
 
-    if (requestAdmins == null || requestMembers == null) return null;
-
-    List<int> decodedMemberIds = decodeIds(
-      requestMembers,
-      'getMembersAndAdminInfo',
-    );
-
-    List<UserForSingleDto> members = await decodeToUserForSingleDtos(
-      decodedMemberIds,
-      token,
-      'getMembersAndAdminInfo',
-    );
-
-    List<int> adminIds = decodeIds(requestAdmins, 'getMembersAndAdminInfo');
-
-    return MembersAndAdminsDto(members, adminIds);
+  GroupRepository(IAuthRepository authRepo) {
+    _authRepository = authRepo;
   }
 
-  static Future<List<UserForSingleDto>> decodeToUserForSingleDtos(
-      decodedMemberIds, token, text) async {
-    List<UserForSingleDto> members = [];
+  @override
+  Future<ErrorStatus> addGroup(String name) async {
+    var token = await _authRepository.getToken();
+    if (token == null) return null;
 
-    for (var memberId in decodedMemberIds) {
-      var response = await User.getUser(memberId, token);
+    Uri uri = Uri.http(kApiUrl, '/api/users/${token.userId}/group/add');
 
-      var userFromJson;
-
-      try {
-        userFromJson = jsonDecode(response);
-      } catch (e) {
-        print('GroupRepository.$text,'
-            ' jsonDecode to List<UserForSingleDto>: $e');
-        return null;
-      }
-
-      members.add(UserForSingleDto.fromJson(userFromJson));
-    }
-
-    return members;
-  }
-
-  static Future<MembersAndFriendsDto> getFriendsAndMembers(
-      userId, groupId, token) async {
-    var requestUsers = await User.getFriends(userId, token);
-    var requestMembers = await Group.getMembersForGroup(userId, groupId, token);
-
-    if (requestMembers == null || requestUsers == null) return null;
-
-    List<UserForSingleDto> friends = [];
+    http.Response response;
 
     try {
-      var decoded = jsonDecode(requestUsers);
-
-      for (var item in decoded) {
-        friends.add(UserForSingleDto.fromJson(item));
-      }
+      response = await http.post(
+        uri,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ${token.token}',
+        },
+        body: jsonEncode(
+          {
+            'name': name,
+          },
+        ),
+      );
     } catch (e) {
-      print('GroupRepository.getFriendsAndMembers,'
-          ' jsonDecode to List<UserForSingleDto>: $e');
       return null;
     }
 
-    List<int> memberIds = decodeIds(requestMembers, 'getFriendsAndMembers');
+    if (response.statusCode == 400) {
+      return new ErrorStatus(
+        decodeAddGroupErrors(response),
+        response.statusCode,
+      );
+    }
 
-    return MembersAndFriendsDto(friends, memberIds);
+    return new ErrorStatus(null, response.statusCode);
   }
 
-  static List<int> decodeIds(data, text) {
-    List<int> ids = [];
+  List<String> decodeAddGroupErrors(http.Response response) {
+    var decoded;
 
     try {
-      List decoded = jsonDecode(data);
-
-      for (var item in decoded) {
-        ids.add(item);
-      }
+      decoded = jsonDecode(response.body);
     } catch (e) {
-      print('GroupRepository.$text, converting adminIds: $e');
+      print(e);
+    }
+
+    List<String> errors;
+
+    if (decoded != null) {
+      decoded = decoded['errors'];
+      if (decoded['Name'] != null) {
+        errors.addAll(decoded['Name']);
+      }
+    } else {
+      errors.add(response.body);
+    }
+
+    return errors;
+  }
+
+  @override
+  Future<ErrorStatus> addMembersToGroup(int groupId, List<int> userIds) async {
+    var token = await _authRepository.getToken();
+    if (token == null) return null;
+
+    Uri uri = Uri.http(
+      kApiUrl,
+      '/api/users/${token.userId}/group/add/$groupId',
+    );
+
+    http.Response response;
+
+    try {
+      response = await http.post(uri,
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ${token.token}',
+          },
+          body: jsonEncode({
+            'ids': userIds,
+          }));
+    } catch (e) {
+      print('Group.addMemberToGroup http request: $e');
       return null;
     }
 
-    return ids;
+    return new ErrorStatus(null, response.statusCode);
+  }
+
+  @override
+  Future<ErrorStatus> changeAdminStatus(int groupId, int memberId) async {
+    var token = await _authRepository.getToken();
+    if (token == null) return null;
+
+    Uri uri = Uri.http(kApiUrl, '/api/users/${token.userId}/group/admin');
+
+    http.Response response;
+
+    try {
+      response = await http.patch(uri,
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ${token.token}',
+          },
+          body: jsonEncode({
+            'userId': memberId,
+            'groupId': groupId,
+          }));
+    } catch (e) {
+      print('Group.changeAdminStatus http request: $e');
+      return null;
+    }
+
+    return new ErrorStatus(null, response.statusCode);
+  }
+
+  @override
+  Future<ErrorStatus> changeGroupName(int groupId, String name) async {
+    var token = await _authRepository.getToken();
+    if (token == null) return null;
+
+    Uri uri = Uri.http(kApiUrl, '/api/users/${token.userId}/group');
+
+    http.Response response;
+
+    try {
+      response = await http.patch(
+        uri,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ${token.token}',
+        },
+        body: jsonEncode(
+          {
+            'id': groupId,
+            'name': name,
+          },
+        ),
+      );
+    } catch (e) {
+      return null;
+    }
+
+    return new ErrorStatus(null, response.statusCode);
+  }
+
+  @override
+  Future<List<int>> getAdminsForGroup(int groupId) async {
+    var token = await _authRepository.getToken();
+    if (token == null) return null;
+
+    Uri uri = Uri.http(
+      kApiUrl,
+      '/api/users/${token.userId}/group/members/admins/$groupId',
+    );
+
+    http.Response response;
+
+    try {
+      response = await http.get(uri, headers: {
+        'Authorization': 'Bearer ${token.token}',
+      });
+    } catch (e) {
+      print('Group.getAdminsForGroup http request: $e');
+      return null;
+    }
+
+    if (response.statusCode != 200) return null;
+
+    return _decodeIds(response);
+  }
+
+  @override
+  Future<List<int>> getMembersForGroup(int groupId) async {
+    var token = await _authRepository.getToken();
+    if (token == null) return null;
+
+    Uri uri = Uri.http(
+      kApiUrl,
+      '/api/users/${token.userId}/group/members/id/$groupId',
+    );
+
+    http.Response response;
+
+    try {
+      response = await http.get(uri, headers: {
+        'Authorization': 'Bearer ${token.token}',
+      });
+    } catch (e) {
+      return null;
+    }
+
+    if (response.statusCode != 200) return null;
+
+    return _decodeIds(response);
+  }
+
+  List<int> _decodeIds(http.Response response) {
+    try {
+      return jsonDecode(response.body);
+    } catch (e) {
+      return null;
+    }
+  }
+
+  @override
+  Future<MembersAndFriends> getFriendsAndMembers(int groupId) async {
+    // TODO: implement getFriendsAndMembers
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<MembersAndAdmins> getMembersAndAdmins(int groupId) async {
+    // TODO: implement getFriendsAndAdmins
+    throw UnimplementedError();
   }
 }
